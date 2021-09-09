3122e9e5549f6e3055a749a1d1628732a722b186
Opilane TTHK <opilane@tthk.ee>
2021-09-09 10:32:35 +0300

    Add Control Flow

---
+++ DrivingLicense/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DrivingLicense
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja sünniaastat
+            //programm kontrollib kasutaja vanust
+            //programm teatab kasutajat, kas ta on piisavalt vana,
+            //et juhiluba saada (tingimuseks 18 aastat vana)
+
+            Console.WriteLine("Mis aastal sündisid?");
+            int Sünniaasta = Convert.ToInt32(Console.ReadLine());
+            int Age = 2021 - Sünniaasta;
+
+
+            if (Age > 18)
+            {
+                Console.WriteLine("Saad teha juhiload.");
+            }
+            else if (Age < 18)
+            {
+                Console.WriteLine("Sa oled liiga noor, et teha juhiload,");
+            }
+            else
+            {
+                Console.WriteLine("Oled täpselt 18, et teha juhiload.");
+            }
+
+            Console.WriteLine("Kena päeva jätku.");
+        }
+    }
+}
---
+++ WhenWereYouBorn/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WhenWereYouBorn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja sünniaastat
+            //programm arvutab kasutaja vanust
+            //programm kuvab kasutaja vanust
+
+            Console.WriteLine("Mis aastal sa sündisid?");
+            int Sünniaasta = Convert.ToInt32(Console.ReadLine());
+            int Vanus = 2021 - Sünniaasta;
+
+            Console.WriteLine($"Sa oled {Vanus} aastat vana.");
+        }
+    }
+}
